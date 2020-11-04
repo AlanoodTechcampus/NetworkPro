0ea87e0a9ba0f7268c66bcfee07676b2a8651b29
reenamtwj <60012179+reenamtwj@users.noreply.github.com>
2020-11-04 08:35:50 +0300

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,19 @@
+public class LinkedList { 
+  
+    Node head; // head of list 
+  
+    // Linked list Node. 
+    // This inner class is made static 
+    // so that main() can access it 
+    static class Node { 
+  
+        int data; 
+        Node next; 
+  
+        // Constructor 
+        Node(int d) 
+        { 
+            data = d; 
+            next = null; 
+        } 
+    } 
